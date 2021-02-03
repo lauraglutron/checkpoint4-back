eac259f20d78750fc19c67b02155fd82a9061040
Laura Glutron <lauraglutron@MacBook-Pro.lan>
2021-02-03 14:37:55 +0100

    add config.js file

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
+const connection = require('./src/config');
 
 const PORT = process.env.PORT || 5000;
-
 const app = express();
 
-app.use(morgan('dev'));
-app.use(cors());
+// To connect to the Database
+connection.connect(function (err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
+});
+
+// Middleware to read json formatted Body request
 app.use(express.json());
 
 // app.use("/", require("./"));
 // app.use("/", require("./"));
 
+// Main route
 app.get('/', (req, res) => {
-  res.send('Welcome to Express');
+  res.send('Welcome to my album application');
 });
 
 app.listen(PORT, () => {
